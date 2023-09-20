872f19c069e54ee0108bf5d829749cf9c982e542
Ravi Choudhary <ravichoudhary141991@gmail.com>
2023-09-20 11:06:48 +0530

    Update main.py
    Agg

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import matplotlib
 import pandas as pd
 
 def main():
-    matplotlib.use('nbAgg')
+    matplotlib.use('Agg')
     load_dotenv()
 
     st.set_page_config(page_title="CSV App", page_icon=":chart:", layout="centered")
